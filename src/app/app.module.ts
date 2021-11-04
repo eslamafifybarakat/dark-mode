70d106018b47372dc19fa8f21e81c76da2639be2
Eslam Afify Barakat <52158422+eslamafifybarakat@users.noreply.github.com>
2021-11-04 15:16:31 +0200

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DarkModeToggleComponent } from './dark-mode-toggle/dark-mode-toggle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DarkModeToggleComponent
   ],
   imports: [
     BrowserModule,
